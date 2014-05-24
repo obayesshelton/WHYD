809cd57d93eee66d08e12eee729b2d4ef2b37b5e
Oliver Bayes-Shelton MacBook Pro <me@bayes-shelton.co.uk>
2014-05-24 16:27:43 +0100

    just the one image

--- wp-content/themes/arch/single-projects.php
+++ wp-content/themes/arch/single-projects.php
@@ -6,22 +6,7 @@
 
 		<h2 class="ptitle">Montauk House</h2>
 
-		<div class="project-info">
-
-			<p>The volume of this house on the Atlantic shore is pulled north, away from the
-			ocean and towards a small valley running along the west side of the plot. The building form flexes to follow this topographical line, allowing the lower deck to engage naturally with the flow of the dune. The material palette reinforces the intimacy of the relationship between architecture and context, with walls finished in sandy render and decks made from silvered ipe, resembling driftwood. A series of pristine vertical planes rises from the upper deck, framing space and views and providing a powerful counterpoint to the structure’s low horizontal mass.</p>
-
-			<p>
-				<strong>Project Architect</strong>
-				<br/>
-				Guy Dickinson, Mark Treharne
-			</p>
-			<p>
-				<strong>Photography</strong>
-				<br/>
-				Gilbert McCarragher
-			</p>
-		</div>
+		<br class="clear"/>
 
 		<div class="project-info">
 
@@ -39,6 +24,25 @@
 				Gilbert McCarragher
 			</p>
 		</div>
+
+		<div class="project-info">
+
+			<p>The volume of this house on the Atlantic shore is pulled north, away from the
+			ocean and towards a small valley running along the west side of the plot. The building form flexes to follow this topographical line, allowing the lower deck to engage naturally with the flow of the dune. The material palette reinforces the intimacy of the relationship between architecture and context, with walls finished in sandy render and decks made from silvered ipe, resembling driftwood. A series of pristine vertical planes rises from the upper deck, framing space and views and providing a powerful counterpoint to the structure’s low horizontal mass.</p>
+
+			<p>
+				<strong>Project Architect</strong>
+				<br/>
+				Guy Dickinson, Mark Treharne
+			</p>
+			<p>
+				<strong>Photography</strong>
+				<br/>
+				Gilbert McCarragher
+			</p>
+		</div>
+
+		<br class="clear"/>
 
 	</div>
 
