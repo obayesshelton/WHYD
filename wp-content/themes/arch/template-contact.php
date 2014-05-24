0925391ec619dea9dc7b5b696d54dce9c7b38526
Oliver Bayes-Shelton MacBook Pro <me@bayes-shelton.co.uk>
2014-05-24 12:48:36 +0100

    added the theme and plugin

---
+++ wp-content/themes/arch/template-contact.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Contact	
+*/	
+?>
+
+<?php get_header(); ?>
+
+	<div class="inside load-item">
+		<div class="page-content">
+
+			<?php get_theme_page_title(); ?>
+			
+			<div class="cpage clear">
+
+				<!--CONTENT-->
+				<div class="contact-desc">
+					
+					<!--MAP-->
+					<div id="map"></div>
+
+				</div>
+
+				<!--FORM-->
+				<div id="respond">
+					<?php
+						if (have_posts()) : 
+							while (have_posts()) : the_post(); 
+								the_content(); 								
+							endwhile; 
+						endif;
+					?>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
+<?php get_footer(); ?>
