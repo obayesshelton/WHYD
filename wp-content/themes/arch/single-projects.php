e8c91e6f451a61ddcbe189c7e91c38d461528781
Oliver Bayes-Shelton MacBook Pro <me@bayes-shelton.co.uk>
2014-05-24 16:08:45 +0100

    just the one image

--- wp-content/themes/arch/single-projects.php
+++ wp-content/themes/arch/single-projects.php
@@ -1,21 +1,25 @@
-<div class="page-content">
+<?php get_header(); ?>
 
-	<h2 class="ptitle">Montauk House</h2>
+	<div class="page-content">
 
-	<div class="services">
+		<h2 class="ptitle">Montauk House</h2>
 
-		Long Island, USA
-		2009 – 2013
+		<div class="services">
 
-		<p>The volume of this house on the Atlantic shore is pulled north, away from the
-		ocean and towards a small valley running along the west side of the plot. The building form flexes to follow this topographical line, allowing the lower deck to engage naturally with the flow of the dune. The material palette reinforces the intimacy of the relationship between architecture and context, with walls finished in sandy render and decks made from silvered ipe, resembling driftwood. A series of pristine vertical planes rises from the upper deck, framing space and views and providing a powerful counterpoint to the structure’s low horizontal mass.</p>
+			Long Island, USA
+			2009 – 2013
 
-		Project Architect
-		Guy Dickinson, Mark Treharne
+			<p>The volume of this house on the Atlantic shore is pulled north, away from the
+			ocean and towards a small valley running along the west side of the plot. The building form flexes to follow this topographical line, allowing the lower deck to engage naturally with the flow of the dune. The material palette reinforces the intimacy of the relationship between architecture and context, with walls finished in sandy render and decks made from silvered ipe, resembling driftwood. A series of pristine vertical planes rises from the upper deck, framing space and views and providing a powerful counterpoint to the structure’s low horizontal mass.</p>
 
-		Photography
-		Gilbert McCarragher
+			Project Architect
+			Guy Dickinson, Mark Treharne
+
+			Photography
+			Gilbert McCarragher
+
+		</div>
 
 	</div>
 
-</div>
+<?php get_footer(); ?>
