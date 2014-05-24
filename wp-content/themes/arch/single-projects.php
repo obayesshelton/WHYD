f2f705cb7c2db450976e1da6de5e98e7db2182a7
Oliver Bayes-Shelton MacBook Pro <me@bayes-shelton.co.uk>
2014-05-24 16:03:08 +0100

    just the one image

--- wp-content/themes/arch/single-projects.php
+++ wp-content/themes/arch/single-projects.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
 
-<div id="content-col" class="supercol col right col-f-537">
-      <div class="col-inner lh-adjust">
-        <div class="works-post">
+<div class="page-content">
 
+	<h2 class="ptitle">Test</h2>
 
-          <div id="royalSliderMiniHolder">
-            <div class="a-left"><div class="a-left-mark">&nbsp;</div>&nbsp;</div>
-            <div class="a-right"><div class="a-right-mark">&nbsp;</div>&nbsp;</div>
-            <div class="a-center"><div class="a-center-mark">&nbsp;</div>&nbsp;</div>
+	<div class="services">
 
-            <div id="royalSliderMini" class="royalSlider rsDefault not-me rsHor" style="height: 358px;">
+		<div id="royalSliderMiniHolder">
+			<div class="a-left"><div class="a-left-mark">&nbsp;</div>&nbsp;</div>
+			<div class="a-right"><div class="a-right-mark">&nbsp;</div>&nbsp;</div>
+			<div class="a-center"><div class="a-center-mark">&nbsp;</div>&nbsp;</div>
+
+			<div id="royalSliderMini" class="royalSlider rsDefault not-me rsHor" style="height: 358px;">
 				<div class="rsOverflow" style="width: 537px; height: 358px;">
 					<div class="rsContainer" style="-webkit-transition: 0s; transition: 0s; -webkit-transform: translate3d(0px, 0px, 0px);">
 						<div style="left: 0px;" class="rsSlide ">
@@ -19,21 +20,21 @@
 					</div>
 				</div>
 			</div>
-          </div>
+		</div>
 
 
-				  <div class="chunk title">
-				    <h2>Montauk House</h2>
-				    <div class="meta-data"><p>Long Island, USA<br>2009 – 2013</p></div>
-				  </div>
-				  <div class="chunk works-body editable">
-				   	<p>The volume of this house on the Atlantic shore is pulled north, away from the <br>ocean and towards a small valley running along the west side of the plot. The building form flexes to follow this topographical line, allowing the lower deck to engage naturally with the flow of the dune. The material palette reinforces the intimacy of the relationship between architecture and context, with walls finished in sandy&nbsp;render and decks made from silvered ipe, resembling driftwood. A series of pristine vertical planes rises from the upper deck, framing space and views and providing a powerful counterpoint to the structure’s&nbsp;low horizontal mass.</p>
-<p class="credit">Project Architect<br>Guy Dickinson, Mark Treharne</p>
-<p class="credit">Photography<br>Gilbert McCarragher</p>
-				   	<p class="credit"></p>
-				   </div>
-				</div>
-      <div id="no-results" style="display: none;">No results</div></div>
-    </div>
+		<div class="chunk title">
+			<h2>Montauk House</h2>
+			<div class="meta-data"><p>Long Island, USA<br>2009 – 2013</p></div>
+		</div>
+		<div class="chunk works-body editable">
+			<p>The volume of this house on the Atlantic shore is pulled north, away from the <br>ocean and towards a small valley running along the west side of the plot. The building form flexes to follow this topographical line, allowing the lower deck to engage naturally with the flow of the dune. The material palette reinforces the intimacy of the relationship between architecture and context, with walls finished in sandy&nbsp;render and decks made from silvered ipe, resembling driftwood. A series of pristine vertical planes rises from the upper deck, framing space and views and providing a powerful counterpoint to the structure’s&nbsp;low horizontal mass.</p>
+			<p class="credit">Project Architect<br>Guy Dickinson, Mark Treharne</p>
+			<p class="credit">Photography<br>Gilbert McCarragher</p>
+			<p class="credit"></p>
+		</div>
+	</div>
+						
+</div>
 
 <?php get_footer(); ?>
