f9bf8177cd394c98d177fdfd3abb418fff1a393b
Oliver Bayes-Shelton MacBook Pro <me@bayes-shelton.co.uk>
2014-05-24 16:50:30 +0100

    just the one image

--- wp-content/themes/arch/single-projects.php
+++ wp-content/themes/arch/single-projects.php
@@ -1,79 +1,94 @@
 <?php get_header(); ?>
 
-		<div class="inside load-item" style="display: block;">
+		<?php
+			if ( post_password_required() ) {
 
-			<div class="page-content">
+			}else {
+				if ( have_posts() ) :
+					while (have_posts()) : the_post();
 
-				<h2 class="ptitle"><?php the_title(); ?></h2>
+		?>
+						<div class="inside load-item" style="display: block;">
 
-				<br class="clear"/>
+							<div class="page-content">
 
-				<div class="project-info">
+								<h2 class="ptitle"><?php the_title(); ?></h2>
 
-					<?php the_content(); ?>
+								<br class="clear"/>
 
-				</div>
+								<div class="project-info">
 
-				<div class="project-info" style="margin-right: 0px;">
+									<?php the_content(); ?>
 
-					<div class="portfolio-flexslider">
-						<ul class="slides">
-							<?php
-								$pattachment_order = get_option('arch_attachment_order');
-								$pattachment_orderby = get_option('arch_attachment_orderby');
+								</div>
 
-								if(get_option('arch_project_thumb_slider')!="true") {
-									$args = array( 'post_type' => 'attachment', 'numberposts' => -1, 'order' => $pattachment_order, 'orderby' => $pattachment_orderby, 'post_status' => null, 'post_parent' => $post->ID );
-								}else {
-									$thumb_id = get_post_thumbnail_id( get_the_ID() );
-									$args = array( 'post_type' => 'attachment', 'numberposts' => -1, 'order' => $pattachment_order, 'orderby' => $pattachment_orderby, 'post_status' => null, 'post_parent' => $post->ID, 'exclude' => $thumb_id );
-								}
+								<div class="project-info" style="margin-right: 0px;">
 
-								$attachments = get_posts( $args );
+									<div class="portfolio-flexslider">
+										<ul class="slides">
+											<?php
+												$pattachment_order = get_option('arch_attachment_order');
+												$pattachment_orderby = get_option('arch_attachment_orderby');
 
-								if ( $attachments ) {
-									foreach ( $attachments as $attachment ) {
-										$img_title = $attachment->post_title;
-										$attachment_id = $attachment->ID;
-										$type = get_post_mime_type( $attachment->ID );
-										$large_image_url = wp_get_attachment_image_src( $attachment_id, 'full' );
+												if(get_option('arch_project_thumb_slider')!="true") {
+													$args = array( 'post_type' => 'attachment', 'numberposts' => -1, 'order' => $pattachment_order, 'orderby' => $pattachment_orderby, 'post_status' => null, 'post_parent' => $post->ID );
+												}else {
+													$thumb_id = get_post_thumbnail_id( get_the_ID() );
+													$args = array( 'post_type' => 'attachment', 'numberposts' => -1, 'order' => $pattachment_order, 'orderby' => $pattachment_orderby, 'post_status' => null, 'post_parent' => $post->ID, 'exclude' => $thumb_id );
+												}
 
-										switch ( $type ) {
-											case 'video/mp4':
-												?>
-												<li>
-													<video id="videojs_gallery" class="video-js vjs-default-skin" controls preload="none" width="728" height="450" poster="" data-setup="{}">
-														<source src="<?php echo wp_get_attachment_url( $attachment->ID ); ?>" type='video/mp4' />
-													</video>
-												</li>
-												<?php
-											break;
-											default:
-												?>
-												<li><?php echo wp_get_attachment_image( $attachment->ID, 'portfolio-large' ); ?></li>
-												<?php
-											break;
-										}
+												$attachments = get_posts( $args );
 
-									}
-								}else {
-									?><img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-large.jpg" alt="" title="" /><?php
-								}
-							?>
-						</ul>
-						<ul class="flex-direction-nav">
-							<li><a class="flex-prev" href="#">Previous</a></li>
-							<li><a class="flex-next" href="#">Next</a></li>
-						</ul>
-					</div>
+												if ( $attachments ) {
+													foreach ( $attachments as $attachment ) {
+														$img_title = $attachment->post_title;
+														$attachment_id = $attachment->ID;
+														$type = get_post_mime_type( $attachment->ID );
+														$large_image_url = wp_get_attachment_image_src( $attachment_id, 'full' );
 
-				</div>
+														switch ( $type ) {
+															case 'video/mp4':
+																?>
+																<li>
+																	<video id="videojs_gallery" class="video-js vjs-default-skin" controls preload="none" width="728" height="450" poster="" data-setup="{}">
+																		<source src="<?php echo wp_get_attachment_url( $attachment->ID ); ?>" type='video/mp4' />
+																	</video>
+																</li>
+																<?php
+															break;
+															default:
+																?>
+																<li><?php echo wp_get_attachment_image( $attachment->ID, 'portfolio-large' ); ?></li>
+																<?php
+															break;
+														}
 
-				<br class="clear"/>
+													}
+												}else {
+													?><img src="<?php echo get_template_directory_uri(); ?>/img/placeholder-large.jpg" alt="" title="" /><?php
+												}
+											?>
+										</ul>
+										<ul class="flex-direction-nav">
+											<li><a class="flex-prev" href="#">Previous</a></li>
+											<li><a class="flex-next" href="#">Next</a></li>
+										</ul>
+									</div>
 
-			</div>
+								</div>
 
-		</div>
+								<br class="clear"/>
+
+							</div>
+
+						</div>
+
+		<?php
+					endwhile;
+
+				endif;
+			}
+		?>
 
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/flexslider/jquery.flexslider-min.js"></script>
 		<script type="text/javascript" src="<?php echo plugins_url(); ?>/typo-shortcodes/js/toggle/jquery.toggle.js"></script>
