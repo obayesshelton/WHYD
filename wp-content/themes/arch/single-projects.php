877452ccca82793660c2a1d40929082cf54fcfed
Oliver Bayes-Shelton MacBook Pro <me@bayes-shelton.co.uk>
2014-05-24 16:24:25 +0100

    just the one image

--- wp-content/themes/arch/single-projects.php
+++ wp-content/themes/arch/single-projects.php
@@ -6,18 +6,17 @@
 
 		<h2 class="ptitle">Montauk House</h2>
 
-		<div class="services">
-
-			Long Island, USA
-			2009 – 2013
-
+		<div class="project-info">
+			
 			<p>The volume of this house on the Atlantic shore is pulled north, away from the
 			ocean and towards a small valley running along the west side of the plot. The building form flexes to follow this topographical line, allowing the lower deck to engage naturally with the flow of the dune. The material palette reinforces the intimacy of the relationship between architecture and context, with walls finished in sandy render and decks made from silvered ipe, resembling driftwood. A series of pristine vertical planes rises from the upper deck, framing space and views and providing a powerful counterpoint to the structure’s low horizontal mass.</p>
 
-			Project Architect
+			<strong>Project Architect</strong>
+			<br/>
 			Guy Dickinson, Mark Treharne
 
-			Photography
+			<strong>Photography</strong>
+			<br/>
 			Gilbert McCarragher
 
 		</div>
