ec6c4028a5e52254be61bba6f13acf9f1cd3805b
Oliver Bayes-Shelton MacBook Pro <me@bayes-shelton.co.uk>
2014-05-24 14:38:08 +0100

    testing a new way to show projects

--- wp-content/themes/arch/loop-projects.php
+++ wp-content/themes/arch/loop-projects.php
@@ -2,12 +2,36 @@
 	if(get_post_meta( $post->ID, "exclude", true )) {
 	}else {
 		?>
-		<div class="chunk">
-			<h2>Montauk House</h2>
-			<div class="meta-data">
-				<p>Long Island, USA<br>2009 – 2013</p>
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div class="link-hover">
+				<a href="<?php the_permalink(); ?>" class="<?php if(get_option('arch_project_hover_link')=="Lightbox") { echo "frame"; } ?>">
+					<?php
+						if ( post_password_required() ) {
+							?>
+							<img src="<?php echo get_template_directory_uri(); ?>/img/password-protect.jpg" alt="" title="" />
+							<?php
+						}else {
+							if ( has_post_thumbnail() ) {
+								the_post_thumbnail('portfolio-medium', array('title' => ''.get_the_title().''));
+							}else {
+								?><img src="<?php echo get_template_directory_uri(); ?>/img/placeholder.jpg" alt="" title="" /><?php
+							}
+						}
+
+						if ( get_post_meta( $post->ID, "video", true ) ) {
+							?><div class="blog-hover"><img src="<?php echo get_template_directory_uri(); ?>/img/icons/video.png" alt="" title="" /></div><?php
+						}else {
+							?><div class="blog-hover"><img src="<?php echo get_template_directory_uri(); ?>/img/icons/link.png" alt="" title="" /></div><?php
+						}
+					?>
+
+				</a>
 			</div>
-		</div>	
+			<div class="project-details">
+				<h3><a href="<?php the_permalink(); ?>" class="<?php if(get_option('arch_project_hover_link')=="Lightbox") { echo "frame"; } ?>"><?php the_title(); ?></a></h3>
+				<span><?php echo get_the_term_list( $post->ID, 'project_categories', ' ', ', ', '' ); ?></span>
+			</div>
+		</div>
 		<?php
 	}
 ?>
