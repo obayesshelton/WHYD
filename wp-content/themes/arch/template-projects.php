640973647a11b85d6acdf92905b25c7b040cc47b
Oliver Bayes-Shelton MacBook Pro <me@bayes-shelton.co.uk>
2014-05-24 14:38:35 +0100

    testing a new way to show projects

--- wp-content/themes/arch/template-projects.php
+++ wp-content/themes/arch/template-projects.php
@@ -24,12 +24,28 @@ Template Name: Projects
 		?>
 
 		<!--PROJECT LIST-->
-		<div class="chunk">
-              <h2>Montauk House</h2>
-              <div class="meta-data">
-                <p>Long Island, USA<br>2009 – 2013</p>
-			</div>
-        </div>
+		<div class="horScroll">
+			<section class="project-list clear">
+				<?php
+					$project_order = get_option('arch_album_order');
+					$project_orderby = get_option('arch_album_orderby');
+
+					/*THIS IS A CODE FOR SPECIFIC CATEGORY SLUG*/
+					//$args = array( 'post_type' => 'projects', 'orderby' => $project_orderby, 'order' => $project_order, 'posts_per_page' => -1, 'tax_query' => array(array('taxonomy' => 'project_categories', 'field' => 'slug', 'terms' => 'web-development' )));
+
+					$args = array( 'post_type' => 'projects', 'orderby' => $project_orderby, 'order' => $project_order, 'posts_per_page' => -1 );
+					$wp_query = new WP_Query( $args );
+
+					if ( $wp_query->have_posts() ) :
+						while ($wp_query->have_posts()) : $wp_query->the_post();
+							get_template_part( 'loop', 'projects' );
+						endwhile;
+					else :
+						_e( 'You should add a record for your Project Post', ENGINE_THEME_NAME );
+					endif;
+				?>
+			</section>
+		</div>
 
 	</div>
 
