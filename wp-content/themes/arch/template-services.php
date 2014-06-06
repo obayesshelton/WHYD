881ee5713a22b62aafc61ec1ec824605221e3cdf
Oliver Bayes-Shelton MacBook Pro <me@bayes-shelton.co.uk>
2014-06-06 12:49:24 +0100

    created a services template

---
+++ wp-content/themes/arch/template-services.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: Services
+*/
+?>
+
+<?php get_header(); ?>
+
+    <div class="inside load-item">
+
+        <div class="page-content">
+
+            <?php get_theme_page_title(); ?>
+
+            <div class="team-list clear">
+
+                <?php
+                    if (have_posts()) :
+                        while (have_posts()) : the_post();
+                            the_content();
+                        endwhile;
+                    endif;
+                ?>
+
+            </div>
+
+        </div>
+
+    </div>
+
+<?php get_footer(); ?>
